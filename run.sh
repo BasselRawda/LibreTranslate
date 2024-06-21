0e85489bdd6d12a79d626055e45ebba26a54da0d
Bassel Rawda <basselrawda96@gmail.com>
2024-06-21 23:05:53 +0200

    a26

--- run.sh
+++ run.sh
@@ -5,13 +5,21 @@ set -eo pipefail
 echo "Starting LibreTranslate..."
 echo "PORT: ${PORT}"
 
-# Start the LibreTranslate service using the Heroku-provided $PORT
-./venv/bin/libretranslate --host 0.0.0.0 --port $PORT
+# Default port to 5000 if not set by Heroku
+PORT=${PORT:-5000}
+
+# More debug information
+echo "Using port: ${PORT}"
+
+# Start the LibreTranslate service and redirect output to a log file
+./venv/bin/libretranslate --host 0.0.0.0 --port $PORT &> /app/libretranslate.log
 
 # Check if LibreTranslate started successfully
 if [ $? -ne 0 ]; then
   echo "Failed to start LibreTranslate"
+  cat /app/libretranslate.log
   exit 1
 fi
 
 echo "LibreTranslate started successfully"
+cat /app/libretranslate.log
